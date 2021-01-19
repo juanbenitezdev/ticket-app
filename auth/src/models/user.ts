827d96ceaf0b14e01d0fea958a28916962e0f9b0
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-01-18 19:40:41 -0500

    feat: defining interfaces to get typescript working correctly with moongose

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,5 +1,16 @@
 import mongoose from 'mongoose';
 
+// An interface that describes the properties that are required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties that a User model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -11,6 +22,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (atrrs: UserAttrs) => {
+  return new User(atrrs);
+};
+
+const User = mongoose.model<any, UserModel>('User', userSchema);
 
 export { User };
