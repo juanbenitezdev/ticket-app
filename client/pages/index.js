cd17586fc721c37e6a88ccd5be4277fb4b249173
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-02-25 16:03:28 -0500

    feat: add api client for axios

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
@@ -6,25 +6,11 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Index</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    // we are on the server since windows only exists on the browser
-    // http://SERVICENAME.NAMESPACE.svc.cluster/api/users/currentuser
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
-
-  return {};
+  return data;
 };
 
 export default LandingPage;
