026ccc9031d796708f70ccf3d8494c103719a9b2
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-02-25 15:52:46 -0500

    feat: using getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,30 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Index</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // we are on the server since windows only exists on the browser
+    // http://SERVICENAME.NAMESPACE.svc.cluster/api/users/currentuser
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+
+  return {};
+};
+
+export default LandingPage;
