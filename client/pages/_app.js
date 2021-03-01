3e4991ce17b9494cf30610edcfc3ee2878d6d82f
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-03-01 15:43:57 -0500

    feat: add header for signin/out

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,31 @@
+// This file is used to import styles or configuration that wants
+// to be applied to the whole app
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  // If we override the app getInitialProps we need to call the components getInitialProps manually
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
