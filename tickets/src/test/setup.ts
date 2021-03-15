8fd3019e06f3b84d15539e918466010c90ecf81e
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-03-15 14:01:31 -0500

    feat: finish tests and endpoint for updating a ticket

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -44,7 +44,7 @@ afterAll(async () => {
 global.signin = () => {
   // Build a JWT payload {id, email}
   const paylod = {
-    id: "15sadfs",
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
