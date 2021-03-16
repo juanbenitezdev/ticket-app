12ecbbb592ca8aa874b23f7fdd6a48ec0e19d4c3
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-03-15 19:58:58 -0500

    feat: add small nats node server test

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,22 @@
+import nats from "node-nats-streaming";
+
+console.clear();
+
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Publisher connected to NATS");
+
+  // data that is passed through the events has to be string
+  const data = JSON.stringify({
+    id: "123",
+    title: "concert",
+    price: 20,
+  });
+
+  stan.publish("ticket:created", data, () => {
+    console.log("Event published");
+  });
+});
