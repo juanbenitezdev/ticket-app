4f120d1372073a28cd41a42a9542601b280e19cc
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-03-20 20:49:04 -0500

    feat: implement a singleton for nats client

---
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketCreatedEvent } from "@juanckets/common";
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
