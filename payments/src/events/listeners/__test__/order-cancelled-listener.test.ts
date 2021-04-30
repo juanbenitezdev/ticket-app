f902edc9583abb7697db69257dbda2d1a6922abd
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-04-29 20:51:37 -0500

    feat: implement new payment services

---
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,51 @@
+import { OrderCancelledEvent, OrderStatus } from "@juanckets/common";
+import mongoose from "mongoose";
+import { Order } from "../../../models/order";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCancelledListener } from "../order-cancelled-listener";
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    price: 10,
+    userId: "asdasd",
+    version: 0,
+  });
+  await order.save();
+
+  const data: OrderCancelledEvent["data"] = {
+    id: order.id,
+    version: 1,
+    ticket: {
+      id: "asdasd",
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg, order };
+};
+
+it("cancels the order", async () => {
+  const { listener, data, msg, order } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
