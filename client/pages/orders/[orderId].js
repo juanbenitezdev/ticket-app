328b9218454634f495a6d4855ef622499114b91a
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-06-29 21:01:48 -0500

    feat: client is finished

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import Router from "next/router";
+import StripeCheckout from "react-stripe-checkout";
+import useRequest from "../../hooks/use-request";
+
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: "/api/payments",
+    method: "post",
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: () => Router.push("/orders"),
+  });
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <br />
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey="pk_test_51Im6w3D2XuBvzIMz1ofYBxgclKoNOdsBprK1PBQaHCeerh8xZLWIrkPR9zG8H4Gvk99p9USqENeeKpPWtnBKQ3WV00C1CyfNF7"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
