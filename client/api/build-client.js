ddeaa757b09cb14040b6a1f427efe35ec8f43cf4
JuanBenitezDev <juanbenitez2908@gmail.com>
2021-07-01 19:44:42 -0500

    fix

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,18 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default ({ req }) => {
-  if (typeof window === 'undefined') {
+  if (typeof window === "undefined") {
     // we are on the server since windows only exists on the browser
     // http://SERVICENAME.NAMESPACE.svc.cluster/api/users/currentuser
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: "ticketing.juanbenitez.dev",
       headers: req.headers,
     });
   } else {
     // we are on the browser
     return axios.create({
-      baseURL: '/',
+      baseURL: "/",
     });
   }
 };
